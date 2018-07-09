a4c9412ce1fb719dff100b7fd7aeb6ebbb997836
Galuh Estya <galuhestya@gmail.com>
2018-07-09 10:58:54 +0700

    implementasi commandline

--- crudmd/crudmd.go
+++ crudmd/crudmd.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"os"
 	"fmt"
 	"log"
+	"flag"
   "strconv"
   "encoding/json"
+	"github.com/joho/godotenv"
 	"github.com/galuhest/item-crud-golang"
 )
 
@@ -14,10 +15,29 @@ type Response struct	{
 	Payload map[string]string
 }
 
+var (
+	envPath = flag.String("env", "", "Path to .env file (default is root)")
+)
+
 func main() {
-  args := os.Args[1:]
-  if len(args) < 2 {
+	var err error
+	flag.Parse()
+	// fmt.Println(*envPath)
+	if *envPath != ""	{
+		err = godotenv.Load(*envPath)
+	} else	{
+		err = godotenv.Load()
+	}
+
+	if err != nil	{
+		log.Fatal("Fail to load .env")
+	}
+
+	var args []string
+  if flag.NArg() < 2 {
   	log.Fatal("Need at least 2 parameters")
+  } else	{
+  	args = flag.Args()
   }
 
   crud := args[0]
@@ -25,17 +45,20 @@ func main() {
   case "get":
   	GetItem(args[1:])
   case "create":
-  	fmt.Println("create")
+  	CreateItem(args[1:])
   case "update":
-  	fmt.Println("update")
+  	UpdateItem(args[1:])
   case "delete":
-  	fmt.Println("delete")
+  	DeleteItem(args[1:])
   default:
   	fmt.Println("invalid argument")
   }
 }
 
 func GetItem(args []string)	{
+	if len(args) != 1	{
+		log.Fatal("Invalid amount of parameters")
+	}
 	id, err := strconv.Atoi(args[0])
 	if err != nil	{
 		log.Fatal("invalid id parameter")
@@ -51,5 +74,53 @@ func GetItem(args []string)	{
 }
 
 func CreateItem(args []string)	{
+	if len(args) != 1	{
+		log.Fatal("Invalid amount of parameters")
+	}
+	name := args[0]
+	db := crud.ConnectDb()
+	crud_response := crud.CreateItem(db, name)
+	data := &Response{Status: "", Payload : nil}
+  err := json.Unmarshal([]byte(crud_response), data)
+  if err != nil {
+  	panic("cant create data")
+  }
+  fmt.Println(data.Payload["id"])
+}
 
+func UpdateItem(args []string)	{
+	if len(args) != 2	{
+		log.Fatal("Invalid amount of parameters")
+	}
+	id, err := strconv.Atoi(args[0])
+	if err != nil	{
+		log.Fatal("invalid id parameter")
+	}
+	name := args[1]
+	db := crud.ConnectDb()
+	crud_response := crud.UpdateItem(db, id, name)
+	data := &Response{Status: "", Payload : nil}
+  err = json.Unmarshal([]byte(crud_response), data)
+  if err != nil {
+  	panic("cant create data")
+  }
+  fmt.Println(data.Status)
+}
+
+func DeleteItem(args []string)	{
+	if len(args) != 1	{
+		log.Fatal("Invalid amount of parameters")
+	}
+	id, err := strconv.Atoi(args[0])
+	if err != nil	{
+		log.Fatal("invalid id parameter")
+	}
+	db := crud.ConnectDb()
+	crud_response := crud.DeleteItem(db, id)
+	data := &Response{Status: "", Payload : nil}
+  err = json.Unmarshal([]byte(crud_response), data)
+  if err != nil {
+  	panic("cant create data")
+  }
+  fmt.Println(data.Status)
 }
